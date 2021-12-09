8cf575960c8dcfef7299426f105d520b1cdd59ab
Remi <91561406+SeniorRemi@users.noreply.github.com>
2021-12-09 23:56:37 +0300

    Java 2 HomeWork 2

---
+++ MyArrayDataException.java
@@ -0,0 +1,14 @@
+public class MyArrayDataException  extends  NumberFormatException{
+
+    static boolean testExceptionData(String [][] arr, int a, int b) throws NumberFormatException{
+
+        try {
+            Integer.parseInt(arr[a][b]);
+
+        }catch (NumberFormatException ss){
+            return true;
+
+        }
+        return false;
+    }
+}
